9b6cddc66feed6b9ab5fcc6eb42825ff12e2eb42
Kenneth Fukizi <kennethfukizi@ymail.com>
2018-02-26 11:26:22 +0200

    First stubs for an experimental chat app

---
+++ Fukizi.MChat/Fukizi.MChat/Fukizi.MChat/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Fukizi.MChat
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new Fukizi.MChat.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
